f4ee464aec4220e5d654cd115d72976f68caabdb
Aparna Shaju <aparnakanatil2002@gmail.com>
2025-06-02 17:35:37 +0530

    error solved

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>Typescript</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>MongoDB</li>
+        <li>DSA</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>B.Tech Computer Science and Engineering
+        </li>
+        
+      </ul>
+    ),
+  },
+  // {
+  //   title: "Certifications",
+  //   id: "certifications",
+  //   content: (
+  //     <ul className="list-disc pl-2">
+  //       <li>AWS Cloud Practitioner</li>
+  //       <li>Google Professional Cloud Developer</li>
+  //     </ul>
+  //   ),
+  // },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/AboutImage.jpg" alt="about image" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">💼 About Me</h2>
+          <p className="text-base lg:text-lg text-justify">
+  I am a passionate MERN Stack Developer with a strong focus on building scalable, user-friendly web applications. Skilled in developing full-stack solutions using MongoDB, Express.js, React.js, and Node.js, I enjoy creating responsive UIs and robust backend services that solve real-world problems. <br/>
+
+  I have successfully built Lush Aura, a feature-rich eCommerce platform with product listings, user authentication, cart functionality, and payment integration. Currently, I&apos;m working on Planzo, an event management web application that helps users seamlessly plan and manage events through an intuitive, organized interface.<br/>
+
+  I thrive in collaborative environments, love clean and maintainable code, and continuously seek to grow my skills in full-stack and cloud-based development.
+</p>
+
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            {/* <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton> */}
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/page.js
@@ -0,0 +1,29 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
+import TechStackSection from "./components/TechStackSection";
+import Skills from "./components/Skills";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        {/* <AchievementsSection /> */}
+        <AboutSection />
+        
+        
+        <ProjectsSection />
+        <Skills/>
+        <TechStackSection/>
+        <EmailSection />
+        
+      </div>
+      <Footer />
+    </main>
+  );
+}
