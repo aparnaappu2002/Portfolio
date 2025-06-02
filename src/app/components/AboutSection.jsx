8136d44bb789f174aa3c1824ca63883e621aa291
Aparna Shaju <aparnakanatil2002@gmail.com>
2025-06-02 21:28:48 +0530

    image changed

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -45,7 +45,7 @@ const AboutSection = () => {
   return (
     <section className="text-white " id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/aboutImage.jpg"  alt="about image" width={500} height={500} />
+        <Image src="/images/20240803_155852.jpg"  alt="about image" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold  mb-4 bg-gradient-to-r from-pink-400 via-purple-400  to-red-400 bg-clip-text text-transparent">About Me</h2>
           <p className="text-base lg:text-lg text-justify">
