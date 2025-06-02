7dc79cd2b752c9e5274d5e3039014a29445c7852
Aparna Shaju <aparnakanatil2002@gmail.com>
2025-06-02 21:06:43 +0530

    image changed

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,14 +9,12 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>SQL</li>
-        <li>Typescript</li>
+        <li>React JS</li>
+        <li>Node JS</li>
+        <li>Mongo DB</li>
+        <li>Express JS</li>
         <li>JavaScript</li>
-        <li>React</li>
-        <li>MongoDB</li>
-        <li>DSA</li>
+        <li>Tailwind CSS</li>
       </ul>
     ),
   },
@@ -25,22 +23,13 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>B.Tech Computer Science and Engineering
-        </li>
+        <li>B.Tech in Computer Science and Engineering </li>
+        
         
       </ul>
     ),
-  },
-  // {
-  //   title: "Certifications",
-  //   id: "certifications",
-  //   content: (
-  //     <ul className="list-disc pl-2">
-  //       <li>AWS Cloud Practitioner</li>
-  //       <li>Google Professional Cloud Developer</li>
-  //     </ul>
-  //   ),
-  // },
+  }
+  
 ];
 
 const AboutSection = () => {
@@ -50,23 +39,22 @@ const AboutSection = () => {
   const handleTabChange = (id) => {
     startTransition(() => {
       setTab(id);
-    });
+    }); 
   };
 
   return (
-    <section className="text-white" id="about">
+    <section className="text-white " id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/AboutImage.jpg" alt="about image" width={500} height={500} />
+        <Image src="/images/aboutImage.jpg"  alt="about image" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">💼 About Me</h2>
+          <h2 className="text-4xl font-bold  mb-4 bg-gradient-to-r from-pink-400 via-purple-400  to-red-400 bg-clip-text text-transparent">About Me</h2>
           <p className="text-base lg:text-lg text-justify">
-  I am a passionate MERN Stack Developer with a strong focus on building scalable, user-friendly web applications. Skilled in developing full-stack solutions using MongoDB, Express.js, React.js, and Node.js, I enjoy creating responsive UIs and robust backend services that solve real-world problems. <br/>
+           I&apos;m a passionate MERN Stack Developer with a strong focus on building scalable, user-friendly web applications. Skilled in developing full-stack solutions using MongoDB, Express.js, React.js, and Node.js, I enjoy creating responsive UIs and robust backend services that solve real-world problems.
+</p> <br />
+<p className="text-base lg:text-lg text-justify">I have successfully built Lush Aura, a feature-rich eCommerce platform with product listings, user authentication, cart functionality, and payment integration. Currently, I'm working on Planzo, an event management web application that helps users seamlessly plan and manage events through an intuitive, organized interface.</p> <br />
 
-  I have successfully built Lush Aura, a feature-rich eCommerce platform with product listings, user authentication, cart functionality, and payment integration. Currently, I&apos;m working on Planzo, an event management web application that helps users seamlessly plan and manage events through an intuitive, organized interface.<br/>
-
-  I thrive in collaborative environments, love clean and maintainable code, and continuously seek to grow my skills in full-stack and cloud-based development.
-</p>
-
+<p className="text-base lg:text-lg text-justify">I thrive in collaborative environments, love clean and maintainable code, and continuously seek to grow my skills in full-stack and cloud-based development.</p>
+          
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
